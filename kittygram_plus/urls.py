d96a9465a986c9d7357bf53f3b7401552f795900
Dmitry Ant <dmimev@gmail.com>
2025-01-23 17:28:27 +0300

    last changes

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -10,4 +10,6 @@ router.register('cats', CatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
